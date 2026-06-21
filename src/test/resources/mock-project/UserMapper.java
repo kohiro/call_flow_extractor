08f60484d27a978c8407c6e70f073e5dcb88b922
kobayashi <kobayashi@ccy.jp>
2026-06-21 09:58:53 +0900

    test: Add XML mapping for UserMapper to test XML extraction

--- src/test/resources/mock-project/UserMapper.java
+++ src/test/resources/mock-project/UserMapper.java
@@ -1,7 +1,6 @@
 package mock;
 
 import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 import java.util.Optional;
 
 public interface UserMapper {
@@ -9,6 +8,6 @@ public interface UserMapper {
     @Select("SELECT * FROM users WHERE id = #{id}")
     Optional<User> findById(String id);
 
-    @Update("UPDATE users SET name = 'foo' WHERE id = #{requestedId}")
+    // SQL is defined in UserMapper.xml
     User updateUser(UserRequest request);
 }
